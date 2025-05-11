2da22cba5ba03d2a4eb634428f77e353d51a9012
lkw <zgbtlkw@gmail.com>
2025-05-11 03:59:57 +0000

    health

---
+++ src/health-reframe.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 're-health-1',
+        keyword: '',
+        sentence: 'Usual Frame: When I am hungry, I eat food. Reframe: Some food is fuel. Some food is entertainment.',
+        chinese: '通常的框架：当我饿了，我就吃东西。重新定义：有些食物是燃料。有些食物是娱乐。'
+    },
+    {
+        id: 're-health-2',
+        keyword: '',
+        sentence: 'Usual Frame: Overeating is a willpower problem. Reframe: Overeating is a knowledge problem. Willpower isn\'t real.',
+        chinese: ''
+    },
+    {
+        id: 're-health-3',
+        keyword: '',
+        sentence: 'Usual Frame: I want food, so I must be hungry. Reframe: I want food, so I might be tired.',
+        chinese: ''
+    },
+    {
+        id: 're-health-4',
+        keyword: '',
+        sentence: 'Usual Frame: I\'m hungry, so I need food. Reframe: I\'m hungry, so I need protein.',
+        chinese: ''
+    },
+    {
+        id: 're-health-5',
+        keyword: '',
+        sentence: 'Usual Frame: Exercising requires willpower and motivation. Reframe: Exercising is easier than not exercising if you turn it into a habit.',
+        chinese: ''
+    },
+    {
+        id: 're-health-6',
+        keyword: '',
+        sentence: 'Usual Frame: It\'s important to learn the best ways to exercise. Reframe: The best exercises are the ones you are willing to do.',
+        chinese: ''
+    },    
+]
