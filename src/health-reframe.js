692deca2887dabcdf5283e9a451a36d461f34b3d
lkw <zgbtlkw@gmail.com>
2025-05-14 07:59:46 +0000

    a

--- src/health-reframe.js
+++ src/health-reframe.js
@@ -34,29 +34,7 @@ export const data = [
         keyword: '有效运动',
         sentence: 'Usual Frame: It\'s important to learn the best ways to exercise. Reframe: The best exercises are the ones you are willing to do.',
         chinese: '通常的框架：学习最佳运动方式很重要。重新定义：最好的运动是你愿意去做的运动。'
-    },
-    {
-        id: 're-s-16',
-        keyword: '最后',
-        sentence: 'For in the end, he was trying to tell us that what afflicted the people in Brave New World was not that they were laughing instead of thinking,',
-        chinese: '最后，他试图告诉我们，《美丽新世界》中的人们所受的困扰，不是他们笑而不是思考'
-    },
-    {
-        id: 're-s-17',
-        keyword: '取代',
-        sentence: 'but that they did not know what they were laughing about and why they had stopped thinking.',
-        chinese: '而是他们不知道自己在笑什么，也不知道为什么停止了思考。'
-    },
-    {
-        id: 're-s-18',
-        keyword: '伟大',
-        sentence: 'Greatness starts with the replacement of hatred with polite disdain.',
-        chinese: '伟大始于用礼貌的蔑视取代仇恨'
-    },
-    {
-        id: 're-s-19',
-        keyword: '谦卑',
-        sentence: 'True humility is when you can surprise yourself more than others; the rest is either shyness or good marketing.',
-        chinese: '真正的谦卑是你能让自己比别人更惊讶；剩下的要么是害羞，要么是良好的营销。'
     }
+    
 ]
+
