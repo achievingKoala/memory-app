50945194ad3b7abce7fef8625bb1a10283d8334a
lkw <zgbtlkw@gmail.com>
2025-05-31 09:06:16 +0000

    add-ran

---
+++ src/random-2025.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        id: 't1-8',
+        keyword: '现代性',
+        sentence: 'Modernity\'s double punishment is to make us both age prematurely and live longer.',
+        chinese: '现代性的双重惩罚是让我们既早衰又活得更久。'
+    },
+    {
+        id: 't1-14',
+        keyword: '工作',
+        sentence: 'Work destroys your soul by stealthily invading your brain during the hours not officially spent working; be selective about professions.',
+        chinese: '工作通过悄悄侵入你的大脑，在你非正式工作的时间里摧毁你的灵魂；选择职业时要有选择性。'
+    },
+    {
+        id: 't2-11',
+        keyword: '临时',
+        sentence: 'Nothing is more permanent than "temporary" arrangements, deficits, truces, and relationships; and nothing is more temporary than "permanent" ones.',
+        chinese: '没有什么比“临时”的安排、赤字、休战和关系更持久的；也没有什么比“永久”的更短暂的。',
+        hide: 0
+    },
+    {
+        id: 't2-15',
+        keyword: '失败者',
+        sentence: 'The characteristic feature of the loser is to bemoan, in general terms, mankind\'s flaws, biases, contradictions, and irrationality - without exploiting them for fun and profit.',
+        chinese: '失败者的特点是对人类的缺陷、偏见、矛盾和非理性表示哀叹——却不利用它们来娱乐和获利。',
+        hide: 0
+    },
+]
+// In science you need to understand the world; in business you need others to misunderstand it.
+// An idea starts to be interesting when you get scared of taking it to its logical conclusion.
+// I suspect that they put Socrates to death because there is something terribly unattractive, alienating, and nonhuman in thinking with too much clarity.
+// Economics cannot digest the idea that the collective (and the aggregate) are disproportionately less predictable than individuals.
