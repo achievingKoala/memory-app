3f191ff49e78c2a7b41cbe794809d064aa35dba1
lkw <zgbtlkw@gmail.com>
2025-06-08 05:32:30 +0000

    add

--- src/random-2025.js
+++ src/random-2025.js
@@ -25,6 +25,56 @@ export const data = [
         chinese: '失败者的特点是对人类的缺陷、偏见、矛盾和非理性表示哀叹——却不利用它们来娱乐和获利。',
         hide: 0
     },
+    {
+        id: 't2-ep1',
+        keyword: '天才',
+        sentence: 'The imagination of the genius vastly surpasses his intellect; the intellect of the academic vastly surpasses his imagination.',
+        chinese: '天才的想象力远远超过他的学问，书呆子的学问远远超过他的想象力',
+        hide: 0
+    },
+    {
+        id: 't2-ro1',
+        keyword: '强健',
+        sentence: 'For the robust, an error is information; for the fragile, an error is an error. Robustness is progress without impatience.',
+        chinese: '对于强健的人来说，错误是信息；对于脆弱的人来说，错误是错误。强健是没有不耐烦的进步。',
+        hide: 0
+    },
+    {
+        id: 'r2-pa1',
+        keyword: '转移',
+        sentence: 'Skills that transfer: street fights, off-path hiking, seduction, broad erudition. Skills that don\'t: school, games, sports, laboratory - what\'s reduced and organized.',
+        chinese: '转移：街头格斗、离经叛道的徒步、诱惑、广泛的知识。不转移：学校、游戏、运动、实验室 - 什么被减少了和组织化了。',
+        hide: 0
+    },
+    {
+        id: 'r2-pa1',
+        keyword: '缺陷',
+        sentence: 'A genius is someone with flaws harder to imitate than his qualities.',
+        chinese: '天才是一个比他的品质更难模仿的缺陷的人。',
+        hide: 0
+    },
+    {
+        id: 'win30-1',
+        keyword: '未来',
+        sentence: 'If you can imagine the future being brighter, it lifts your energy and gooses the chemistry in your body that produces a sensation of happiness.',
+        chinese: '如果能想象未来更光明，它会提升你的能量，刺激体内产生幸福感的化学物质。',
+        hide: 1
+    },
+    {
+        id: 'random-fool-1',
+        keyword: '操作',
+        sentence: 'Unless we manipulate our surroundings, we have as little control over what and whom we think about as we do over the muscles of our hearts.',
+        chinese: '除非我们操控周围的环境，否则根本没法控制自己去想哪些事、哪些人，就像没法控制自己心脏的跳动一样。' ,
+        hide: 0
+    },
+    {
+        id: 're-introducing-2',
+        keyword: '洗澡',
+        sentence: 'Your brain is most intelligent when you don\'t instruct it on what to do - something people who take showers discover on occasion.',
+        chinese: '当你不指示你的大脑做什么时，你的大脑是最聪明的——这是洗澡的人偶尔发现的。',
+        hide: 0
+    },
+    
 ]
 // In science you need to understand the world; in business you need others to misunderstand it.
 // An idea starts to be interesting when you get scared of taking it to its logical conclusion.
